4b811d7ccd03a244a414e7e34a291eb7e4296ab4
Rafael Rodriguez <rafart1205@gmail.com>
2022-11-24 02:17:11 -0300

    Calculadora01
    Falta la parte de ingresar los numeros y operadores por lo botones del html y que haga la operacion

--- calcujsrt02.js
+++ calcujsrt02.js
@@ -15,67 +15,40 @@ function calculate() {
   calcu.appendChild(resultadoFinal)
 
   cuerpo.appendChild(calcu)
+
+  resultado = prompt();
+  switch (operadores) {
+    case "+":
+      operadores = valueant + valuepost;
+      break;
+    case "-":
+      if (valueant > valuepost) {
+        operadores = valueant - valuepost;
+      } else {
+        operadores = valuepost - valueant;
+      }
+      break;
+    case "/":
+      if (valuepost = 0) {
+        mensaje = "Error! No se puede dividir entre cero"
+      } else {
+        operadores = valueant / valuepost
+      }
+      break;
+    case "*":
+      operadores = valueant * valuepost;
+      break;
+    default:
+      mensaje = "Ingrese solo numeros por favor";
+      break;
 }
 
-function ingresoNumAnt() {
-
+function ingresoNum() {
+  let numeros = document.getElementsByClassName("numero")
 }
 
-function ingresoNumPost() {
-
+function ingresoOperador() {
+  let operadores = document.getElementsByClassName("operador")
 }
 
-
-
-let numeros = document.getElementsByClassName("numero")
-let operadores = document.getElementsByClassName("operador")
-
-
-
-
-
-
-
-
-valueant.textContent = appendChild(numeros);
-valuepost.textContentt = appendChild(numeros);
-resultado.textContent = prompt();
-switch (operadores) {
-  case "+":
-    operadores = valueant + valuepost;
-    break;
-  case "-":
-    if (valueant > valuepost) {
-      operadores = valueant - valuepost;
-    } else {
-      operadores = valuepost - valueant;
-    }
-    break;
-  case "/":
-    if (valuepost = 0) {
-      mensaje = "Error! No se puede dividir entre cero"
-    } else {
-      operadores = valueant / valuepost
-    }
-    break;
-  case "*":
-    operadores = valueant * valuepost;
-    break;
-  default:
-    mensaje = "Ingrese solo numeros por favor";
-    break;
 }
-
-
-
-
-
-
-
-resultadoFinal.textContent = resultado.value
-
-
-
-let valueant = document.getElementById("value-ant")
-let valuepost = document.getElementById("value-post")
-let resultado = document.getElementById("resultado")
